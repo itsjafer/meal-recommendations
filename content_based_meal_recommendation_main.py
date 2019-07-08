a3aeefbf6946b5bf7aa11aea887164de98234091
Jafer Haider <itsjafer@gmail.com>
2019-07-08 14:42:12 -0700

    Fully working content based meal recommendation proof of concept

--- content_based_meal_recommendation_main.py
+++ content_based_meal_recommendation_main.py
@@ -7,6 +7,11 @@ from sklearn.preprocessing import MultiLabelBinarizer
 
 
 def train():
+    """Trains the model on ALL preferences offline
+    
+    Returns:
+        DataFrame -- The Trained model
+    """
     print("Training model...")
 
     # Load data and set id as our index
@@ -56,18 +61,20 @@ def train():
     return df_predict.copy()
 
 
-def predict(df_predict, user_no):
+def predict(df_predict, df_food_data, user_no):
+    """ Predict the top 10 recipes for a user
+    
+    Arguments:
+        df_predict {DataFrame} -- the Model for user preferences 
+        user_no {int} -- the user to predict for
+    
+    Returns:
+        DataFrame -- A list of recipes
+    """
     print('Predicting for user id, ' + str(user_no))
-    # Load data
-    df_food_data = pd.read_json(os.path.join(os.getenv("DATA_SCIENCE_DIR"), 
-                                        "content_based_meal_recommendations", "data", "train.json"))
-    df_food_data.set_index('id', inplace=True)
-    df_food_data = df_food_data.head(1000)
 
     #user predicted rating to all books
-    if (user_no not in df_predict.columns.values):
-        return cold_start(df_food_data.sample(n=3), user_no)
-    user_predicted_rating = df_predict[user_no]
+    user_predicted_rating = df_predict[int(user_no)]
 
     #combine book rating and book detail
     user_rating_food = pd.concat([user_predicted_rating,df_food_data], axis=1)
@@ -76,47 +83,136 @@ def predict(df_predict, user_no):
     return user_rating_food.sort_values(by=[user_no], ascending=False).iloc[0:10]
 
 
-# TODO: cold_start should update online such that we don't have to retrain the entire model
-def cold_start(df_food_data, user_no):
+def cold_start(sample_data, df_food_data, user_no):
     """ In content-based models, the cold start problem are usually solved in one of the following ways:
             1. The user is recommended the most popular items 
             2. The user is recommended things based on other metadata (program, location)
             3. The user is given several recipes and asked to rate them
         For this implementation, we will choose option 3.
     """
-    preferences = list()
-    print("\nWe don't have any information about you. Please answer a few questions.")
-    for index, row in df_food_data.iterrows():
+
+    df_user_ratings = pd.DataFrame(columns=['user_id', 'food_id', 'rating'])
+    print("\nPlease answer a few questions.")
+    for index, row in sample_data.iterrows():
         print(row)
         rating = input("\nWhat do you think of the recipe above? 1 = like, 0 = neutral, -1 = dislike\n")
-        preference = (user_no, index, int(rating))
-        preferences.append(preference)
+        df_user_ratings = df_user_ratings.append({'rating': int(rating), 'food_id': int(index), 'user_id':int(user_no)}, ignore_index=True)
 
-    # Update the user preference csv
-    # TODO: Transition to online model so this isn't necessary
+    df_predict = update_preferences(df_food_data, df_user_ratings, int(user_no))
+
+    return predict(df_predict, df_food_data, user_no)
+
+
+def update_preferences(df_food_data, user_pref_df, user_id):
+    """This will update the parameter values for a user based on a new recipe rating
+    
+    Arguments:
+        user_id {int} -- The id of the user whose preference to update
+        recipe_id {int} -- The id of the recipe that is being rated
+        rating {int} -- either -1, 0, or 1
+    
+    Returns:
+        DataFrame -- The updated model with all preferences
+    """
+    print("Updating model...")
+
+    # One Hot Encoding
+    df_ingredient = df_food_data['ingredients']
+    mlb = MultiLabelBinarizer()
+    df_ingredient = pd.DataFrame(mlb.fit_transform(df_ingredient),columns=mlb.classes_, index=df_food_data.index)
+    
+    # TODO: we need to find the normalized term frequency of each recipe-ingredient (square root each term in the matrix)
+    df_ingredient_normalized = df_ingredient
+
+    # Now we will update the user preferences and save it
+    # If the preferences don't exist, we will create a new dataframe
+    df_user_ratings = pd.DataFrame(columns=['user_id', 'food_id', 'rating'])
+    if (os.path.exists(os.path.join(os.getenv("DATA_SCIENCE_DIR"), 
+                                 "content_based_meal_recommendations", "data", "user_preferences.csv"))):
+        df_user_ratings = pd.read_csv(os.path.join(os.getenv("DATA_SCIENCE_DIR"), 
+                                 "content_based_meal_recommendations", "data", "user_preferences.csv"))
+
+    updated_df = df_user_ratings.append(user_pref_df)
+
+    updated_df.to_csv(os.path.join(os.getenv("DATA_SCIENCE_DIR"), 
+                                    "content_based_meal_recommendations", "data", 'user_preferences.csv'), index=False)
+
+    user_pref_df = updated_df.loc[updated_df['user_id'] == user_id]
+    user_pref_df = user_pref_df.astype(int)
+    rating = pd.pivot_table(user_pref_df, values='rating', index=['food_id'], columns = ['user_id']).T
+    rating.fillna(0, inplace=True)
+
+    # We find the user's preferences for each ingredient (create user_profile)
+    df_ingredient_filtered = df_ingredient_normalized[df_ingredient_normalized.index.isin(rating.columns.values)].sort_index()
+
+    user_recipe_matrix = rating.values.dot(df_ingredient_filtered.values)
+    user_preferences = pd.DataFrame(user_recipe_matrix, columns=df_ingredient_filtered.columns, index=rating.index)
+    
+    # Calculate IDF
+    document_frequency = df_ingredient.sum()
+    idf = (len(df_food_data)/document_frequency).apply(np.log)
+    idf_df_item = df_ingredient.mul(idf.values)
+
+    # Find the recipes that most match the user's preferences
+    working_df = idf_df_item.mul(user_preferences.iloc[0], axis=1)
+    df_predict = working_df.sum(axis=1)
+
+
+    # Open the model and update the values for our user
+
+    # If the model doesn't yet exist, we will create it
+    model = pd.DataFrame()
+    if (os.path.exists(os.path.join(os.getenv("DATA_SCIENCE_DIR"), 
+                                        "content_based_meal_recommendations", "data", "tfidf_model.pkl"))):
+        model = pd.read_pickle(os.path.join(os.getenv("DATA_SCIENCE_DIR"), 
+                                    "content_based_meal_recommendations", "data", 'tfidf_model.pkl'))
+    
+    model[int(user_id)] = df_predict
+    
     with open(os.path.join(os.getenv("DATA_SCIENCE_DIR"), 
-                                        "content_based_meal_recommendations", "data", 'user_preferences.csv'),'a') as f:
-        writer = csv.writer(f)
-        writer.writerows(preferences)
+                                        "content_based_meal_recommendations", "data", 'tfidf_model.pkl'), 'wb') as f:
+        pickle.dump(model, f)
 
-    print("Thanks! The model will be retrained with your preferences")
-    df_predict = train()
-    return predict(df_predict, user_no)
+    print('Finished training model!\n')
+    return model.copy()
 
 if __name__ == "__main__":
 
-    #update_preferences(1, 70, -1)
+    # Load data
+    df_food_data = pd.read_json(os.path.join(os.getenv("DATA_SCIENCE_DIR"), 
+                                        "content_based_meal_recommendations", "data", "train.json"))
+    df_food_data.set_index('id', inplace=True)
+    df_food_data = df_food_data.head(1000)
 
-    # train the model based on preferences
-    train()
+    # Get preferences
+    if (not os.path.exists(os.path.join(os.getenv("DATA_SCIENCE_DIR"), 
+                                        "content_based_meal_recommendations", "data", "user_preferences.csv"))):
+        user_id = input("\nPlease enter your user id\n")
+        print(cold_start(df_food_data.sample(n=3), df_food_data, int(user_id)))
+        
+    # Offline train if a model doesn't exist
+    if (not os.path.exists(os.path.join(os.getenv("DATA_SCIENCE_DIR"), 
+                                        "content_based_meal_recommendations", "data", "tfidf_model.pkl"))):
+        train()
 
-    # load the model
-    df_predict = pd.read_pickle(os.path.join(os.getenv("DATA_SCIENCE_DIR"), 
-                                        "content_based_meal_recommendations", "data", 'tfidf_model.pkl'))
-    # print(df_predict)
     while True:
+        # Load the model (in case changes have been made)
+        df_predict = pd.read_pickle(os.path.join(os.getenv("DATA_SCIENCE_DIR"), 
+                                        "content_based_meal_recommendations", "data", 'tfidf_model.pkl'))
+
         user_id = input("\nPlease enter your user id\n")
         if user_id.isnumeric():
-            print(predict(df_predict, int(user_id)))
+            # Check if the user id exists
+            if (int(user_id) not in df_predict.columns.values):
+                # Ask cold start questions and update model online
+                print(cold_start(df_food_data.sample(n=3), df_food_data, int(user_id)))
+                continue
+
+            # If it exists, ask if they want to rate some more recipes or just get predictions
+            if (input("Enter Y if you'd like to rate some more recipes. Press anything else to recieve your recommendations\n") == 'Y'):
+                print(cold_start(df_food_data.sample(n=3), df_food_data, int(user_id)))
+                continue
+            
+            print(predict(df_predict, df_food_data, int(user_id)))
         else:
             break
